f1a6f17563baf1f72b962892f8a98313805cd3a7
kislev-owo <kislev_083@hotmail.com>
2020-09-01 14:53:26 +0000

    actualizacion

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,86 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(20), nullable=False)
+    email = Column(String(30), nullable=False)
+    password = Column(String(50))
+    profile = Column(String())
+    emisor = relationship("Message", backref="author") 
+    receptor = relationship("Message", backref="author") 
+    follower = relationship("Invitations", backref="author") 
+    following = relationship("Invitations", backref="author") 
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Message(Base):
+    __tablename__ = 'message'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    accepted = Column(Boolean())
+    date = Column(DateTime, nullable=False)
+    emisor_id = Column(Integer, ForeignKey('user.id'))
+    receptor_id = Column(Integer, ForeignKey('user.id'))
+
+class Story(Base):
+    __tablename__ = 'story'
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime, nullable=False)
+    photo = Column(String())
+    effects = Column(String())
+    message_id = Column(Integer, ForeignKey('message.id'))
+    message = relationship(Message)        
+ 
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime, nullable=False)
+    photo = Column(String()) 
+    effects = Column(String())
+    description = Column(String())
+    username_id = Column(Integer, ForeignKey('user.id'))
+    username = relationship(User) 
+
+class Comments(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    comments = Column(String())
+    date = Column(DateTime, nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+
+class Likes(Base):
+    __tablename__ = 'likes'
+    id = Column(Integer, primary_key=True)
+    like = Column(String())
+    date = Column(DateTime, nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+   
+class Invitations(Base):
+    __tablename__ = 'invitations'
+    id = Column(Integer, primary_key=True)
+    accepted = Column(Boolean())
+    create_at = Column(DateTime())
+    answered_at = Column(DateTime())
+    follower_id = Column(Integer, ForeignKey('user.id'))
+    followed_id = Column(Integer, ForeignKey('user.id'))
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
+
+
+# ------------------  Importanteeee para que funcione ------------
+# 1 chequear que version tiene $ python3 --version //// el dira cual tiene
+# 2 ir al archivo Pipfile y cambiar python_version por la version q dijo 
+# 3 $ pipenv install
+# 4 $ pipenv shell
+# 5 ahora vas cambiando el codigo en models.py y cuando lo guarde se pone esto
+# 6 $ python src/models.py
+# 7 en diagram.png va a ir cambiando
+# 8 para volver a actualizar ir de paso 5 a paso 7
